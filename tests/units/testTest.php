98dcc4f4b38bd1f435e2e4b478529f21b8607e71
ncourvoi <nicolas.courvoisier@edu.univ-fcomte.fr>
2020-09-25 14:55:25 +0200

    test des tests

---
+++ tests/units/testTest.php
@@ -0,0 +1,15 @@
+<?php
+
+	//Same verifie aussi le type.
+
+	use PHPUnit\Framework\TestCase;
+
+	
+
+	class testTest extends testCase {
+		
+		public function testT () {
+			$this->assertEquals(4,4);
+		}
+		
+	}
