d9e9b8b8b2cddec2e3dfd2ad5b1ee812458363eb
ncourvoi <nicolas.courvoisier@edu.univ-fcomte.fr>
2020-09-29 15:25:01 +0200

    test jenkins test fail

---
+++ tests/units/connexionTest.php
@@ -0,0 +1,17 @@
+<?php
+
+	//Same verifie aussi le type.
+
+	use PHPUnit\Framework\TestCase;
+	
+    include_once __DIR__.'/../../src/PHP/inscription.php';
+
+	class connexionTest extends TestCase {
+
+		public function testJenkins() {
+			$this->assertEquals(0, 1);
+		}
+		
+
+	}
+?>
