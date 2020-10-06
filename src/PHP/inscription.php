45daaf4aaed7b3ea77a62e6161a9fb8b64e45a71
timotheJeannot <timothe.jeannot@gmail.com>
2020-10-06 17:20:05 +0200

    pg15 corr7

--- src/PHP/inscription.php
+++ src/PHP/inscription.php
@@ -211,7 +211,7 @@ function l_verify_data($email, $pass1, $pass2, $nom, $prenom, $username, $naiss_
 
         if($maj == false || $min == false || $spec == false)
         {
-            $err['pass1'] = 'Le mot de passe doit contenir au moin une majuscule, un chiffre et un caractère spécial.';
+            //$err['pass1'] = 'Le mot de passe doit contenir au moin une majuscule, un chiffre et un caractère spécial.';
         }
 
         /*
