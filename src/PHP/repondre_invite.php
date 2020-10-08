eced6e25933201e000e52a4e3cb62176d40b40c5
francis <francis.buamah@gmail.com>
2020-10-08 12:36:12 +0200

    corr repondre_invite

--- src/PHP/repondre_invite.php
+++ src/PHP/repondre_invite.php
@@ -53,7 +53,7 @@ goodle_header();
 			}else{
 			  mysqli_free_result($res);
 			  mysqli_close($bd);
-			  echo '<p>Vous avez deja fait une première reponse à cette invitation </p>',
+			  echo '<p>Vous avez déjà fait une première réponse à cette invitation.</p>',
 			  '<p><a href="../../index.php">Retour à la page d\'accueil</a><p>';
 			  return;
 			}
@@ -91,7 +91,6 @@ goodle_header();
 					echo '<tr><td colspan="4" style="padding-top: 10px;" class="centered">', form_input(Z_SUBMIT,'btnValiderEvent','Valider'), '</td></tr>';
 					echo '<tr><td colspan="4" style="padding-top: 10px;" class="centered">', form_input(Z_HIDDEN,'idInvite', $idInvite), '</td></tr>';
 					echo '<tr><td colspan="4" style="padding-top: 10px;" class="centered">', form_input(Z_HIDDEN,'length', $length), '</td></tr>';
-					echo '<tr><td colspan="4" style="padding-top: 10px;" class="centered">', form_input(Z_HIDDEN,'update', $update), '</td></tr>';
 
 					$listeAEnvoyer = serialize($listeDateEvent);
 					echo '<tr><td colspan="4" style="padding-top: 10px;" class="centered">', form_input(Z_HIDDEN,'listeDateEvent', $listeAEnvoyer), '</td></tr></table>';
