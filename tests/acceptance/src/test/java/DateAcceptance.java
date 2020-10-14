7ed1e1dda93500beaefa7c3f23b6d5bf81ed3d96
Claire Marche <claire.marche@orange.fr>
2020-10-14 13:53:09 +0200

    Added one Acceptance test for tc_180.feature

---
+++ tests/acceptance/src/test/java/DateAcceptance.java
@@ -0,0 +1,31 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.fr.Alors;
+import cucumber.api.java.fr.Et;
+import cucumber.api.java.fr.Etantdonné;
+import cucumber.api.java.fr.Quand;
+
+public class DateAcceptance {
+    @Etantdonné("^l'utilisateur ajoute une proposition de date et heure$")
+    public void lUtilisateurAjouteUnePropositionDeDateEtHeure() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Quand("^la date et l'heure sont suppérieure à la date actuelle$")
+    public void laDateEtLHeureSontSuppérieureÀLaDateActuelle() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Et("^la date et l'heure ne sont pas déjà renseigné$")
+    public void laDateEtLHeureNeSontPasDéjàRenseigné() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Alors("^la date et l'heure sont ajouté à l'événement$")
+    public void laDateEtLHeureSontAjoutéÀLÉvénement() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
