c5d1e118f1adf474370258da9ec25cdd5c02d89e
timotheJeannot <timothe.jeannot@gmail.com>
2020-10-20 11:22:21 +0200

    modif choix date

--- src/PHP/insert_choix_date_event.php
+++ src/PHP/insert_choix_date_event.php
@@ -7,7 +7,7 @@ require_once 'bibli_generale.php';
 error_reporting(E_ALL); // toutes les erreurs sont capturées (utile lors de la phase de développement)
 
 
-$errors = l_controle_piratage_ve();
+//$errors = l_controle_piratage_ve();
 html_debut('Goodle | Voir event', '../src/CSS/styles.css');
 
 goodle_header();
@@ -17,11 +17,11 @@ if (isset($_SESSION['ID'])) {
 
 $bd = bd_connect();
 
-if (count($errors) != 0) {
+/*if (count($errors) != 0) {
     foreach ($errors as $code => $e) {
     echo '<p class="erreur">' . $e . '</p>';
     }
-} else {
+} else {*/
 
     //$sql = "UPDATE Evenement SET DateChoisie =  '$nom', Lieu= '$lieu', DateCloture = $aaaammjj WHERE ID = $idEvent ;";
     $sql = 'UPDATE Evenement SET DateChoisie = '.$_GET['dateEvent'].' WHERE ID = '.$_GET['event'].';';
@@ -29,7 +29,7 @@ if (count($errors) != 0) {
     echo '<p> la date a bien été choisi </p>';
 
     
-}
+//}
 mysqli_close($bd);
 html_fin();
 ob_end_flush();
